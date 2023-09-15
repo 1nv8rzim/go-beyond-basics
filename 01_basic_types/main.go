11cb765e763a767e3753bb6160df26275923df59
1nv8rZim <msf9542@rit.edu>
2023-09-15 18:54:57 -0400

    basic types

---
+++ 01_basic_types/main.go
@@ -0,0 +1,12 @@
+package main
+
+var (
+	Int     int        = -1
+	UInt    uint       = 1
+	Float   float64    = 1.435
+	Complex complex128 = 13.32 + 1.435i
+	Bool    bool       = true
+	String  string     = "Hello World"
+	Byte    byte       = 'a'
+	Rune    rune       = '😳'
+)
